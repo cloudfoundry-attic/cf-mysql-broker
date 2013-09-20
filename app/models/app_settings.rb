6ae70bc0e02c631b7efea3021a7c864cfcd1d6f1
Cornelia Davis and Jeff Schnitzer <pair+cdavisafc+jschnitzer@pivotallabs.com>
2013-09-20 14:12:56 -0700

    Use correct path to app settings.

--- app/models/app_settings.rb
+++ app/models/app_settings.rb
@@ -1,3 +1,7 @@
 class AppSettings < Settingslogic
-  source Rails.root.join("config/app_settings.yml")
+  # This points at /var/vcap/packages, not /var/vcap/jobs which is where
+  # BOSH renders the config templates.
+  #source Rails.root.join("config/app_settings.yml")
+
+  source '/var/vcap/jobs/cf-mysql-broker/config/app_settings.yml'
 end
