6e3d639b724cc493f09b903efb4099073ec6af9c
Jeff Schnitzer and alex choi <pair+jschnitzer+achoi@pivotallabs.com>
2013-09-25 15:23:50 -0700

    Prevents sql injection attacks

--- spec/lib/database_name_spec.rb
+++ spec/lib/database_name_spec.rb
@@ -7,4 +7,16 @@ describe 'DatabaseName' do
     expect(dbname.name).to eq('123_456')
   end
 
+  # Technically we should allow any kind of character in an id;
+  # we don't absolutely know that ids are guids. That would require
+  # writing some escaping code.
+  context 'when there are strange characters in the id' do
+    it 'raises an exception' do
+      expect {
+        DatabaseName.new("!@\#$%^&*()' ;")
+      }.to raise_error(RuntimeError, 'Only ids matching [0-9,a-z,A-Z$-]+ are allowed')
+    end
+
+  end
+
 end
