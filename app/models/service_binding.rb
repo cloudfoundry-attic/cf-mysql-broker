b66c78394a7c377c042cb1f0850d4008986ec1cf
Ian Lesperance and Ryan Tang <pair+ilesperance+ryantang@pivotallabs.com>
2013-10-04 10:24:54 -0700

    Only bother rescuing the user deletion

--- app/models/service_binding.rb
+++ app/models/service_binding.rb
@@ -75,14 +75,14 @@ class ServiceBinding < BaseModel
   def destroy
     begin
       connection.execute("DROP USER '#{username}'")
-
+    rescue ActiveRecord::StatementInvalid => e
+      raise unless e.message =~ /DROP USER failed/
+    else
       # Some MySQL installations, e.g., Travis, seem to need privileges
       # to be flushed even when using the appropriate account management
       # statements, despite what the MySQL documentation says:
       # http://dev.mysql.com/doc/refman/5.6/en/privilege-changes.html
       connection.execute('FLUSH PRIVILEGES')
-    rescue ActiveRecord::StatementInvalid => e
-      raise unless e.message =~ /DROP USER failed/
     end
   end
 
