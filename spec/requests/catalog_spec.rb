d85075c21c9c7bf0f73d3157e05065b23e4c11e7
Matt Royal <mroyal@pivotallabs.com>
2013-11-05 10:49:32 -0800

    Update the settings.yml file
    [#60186036]
    
    Signed-off-by: Sarah Chandler <schandler@pivotallabs.com>

--- spec/requests/catalog_spec.rb
+++ spec/requests/catalog_spec.rb
@@ -11,8 +11,8 @@ describe 'GET /v2/catalog' do
     expect(services).to have(1).service
 
     service = services.first
-    expect(service.fetch('name')).to eq('cf-mysql2')
-    expect(service.fetch('description')).to eq('Cloud Foundry MySQL')
+    expect(service.fetch('name')).to eq('p-mysql')
+    expect(service.fetch('description')).to eq('MySQL service for application development and testing')
     expect(service.fetch('bindable')).to be_true
     expect(service.fetch('metadata')).to eq(
       {
@@ -27,15 +27,15 @@ describe 'GET /v2/catalog' do
     expect(plans).to have(1).plan
 
     plan = plans.first
-    expect(plan.fetch('name')).to eq('free2')
-    expect(plan.fetch('description')).to eq('Free Trial')
+    expect(plan.fetch('name')).to eq('5mb')
+    expect(plan.fetch('description')).to eq('Shared MySQL Server, 5mb persistent disk, 40 max concurrent connections')
     expect(plan.fetch('metadata')).to eq(
       {
-        "cost" => 0.0,
-        "bullets" => [
-          { "content" => "Shared MySQL server" },
-          { "content" => "100 MB storage" },
-          { "content" => "40 concurrent connections" },
+        'cost' => 0.0,
+        'bullets' => [
+          { 'content' => 'Shared MySQL server'},
+          { 'content' => '5 MB storage'},
+          { 'content' => '40 concurrent connections'},
         ]
       }
     )
