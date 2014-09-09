48061e9357fb4bd3468d3f91251ec42b613f5edd
David Sabeti and Ruthie Byers <dsabeti@pivotal.io>
2014-09-08 17:25:57 -0700

    Use up/down syntax for migrations so we can run ActiveRecord code in migration

--- db/migrate/20140616212709_add_db_name_to_service_instances.rb
+++ db/migrate/20140616212709_add_db_name_to_service_instances.rb
@@ -2,7 +2,7 @@ class AddDbNameToServiceInstances < ActiveRecord::Migration
   class ServiceInstance < ActiveRecord::Base
   end
 
-  def change
+  def up
     add_column :service_instances, :db_name, :string
     add_index :service_instances, :db_name
 
@@ -10,5 +10,10 @@ class AddDbNameToServiceInstances < ActiveRecord::Migration
       instance.db_name = "cf_#{instance.guid.gsub('-', '_')}"
       instance.save
     end
+  end
+
+  def down
+    remove_index :service_instances, :db_name
+    remove_column :service_instances, :db_name
   end
 end
