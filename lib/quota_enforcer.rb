40da49d25e990a85295616d8d41693979e50a945
Ross Greenwood and Rob Dimsdale <rdimsdale@pivotal.io>
2014-11-12 17:19:43 -0800

    Fix quota calcuation query.
    [#80639978].

--- lib/quota_enforcer.rb
+++ lib/quota_enforcer.rb
@@ -89,9 +89,8 @@ module QuotaEnforcer
       violators = connection.select_values(<<-SQL)
         SELECT tables.table_schema AS db
         FROM   information_schema.tables AS tables
-        JOIN   mysql.db AS dbs ON tables.table_schema = dbs.Db
+        JOIN   ( SELECT DISTINCT dbs.Db AS Db from mysql.db AS dbs WHERE (dbs.Insert_priv = 'Y' OR dbs.Update_priv = 'Y' OR dbs.Create_priv = 'Y') ) AS dbs ON tables.table_schema = dbs.Db
         JOIN   #{broker_db}.service_instances AS instances ON tables.table_schema = instances.db_name COLLATE utf8_general_ci
-        WHERE  (dbs.Insert_priv = 'Y' OR dbs.Update_priv = 'Y' OR dbs.Create_priv = 'Y')
         GROUP  BY tables.table_schema
         HAVING ROUND(SUM(tables.data_length + tables.index_length) / 1024 / 1024, 1) >= MAX(instances.max_storage_mb)
       SQL
@@ -115,9 +114,8 @@ module QuotaEnforcer
       reformed = connection.select_values(<<-SQL)
         SELECT tables.table_schema AS db
         FROM   information_schema.tables AS tables
-        JOIN   mysql.db AS dbs ON tables.table_schema = dbs.Db
+        JOIN   ( SELECT DISTINCT dbs.Db AS Db from mysql.db AS dbs WHERE (dbs.Insert_priv = 'N' OR dbs.Update_priv = 'N' OR dbs.Create_priv = 'N') ) AS dbs ON tables.table_schema = dbs.Db
         JOIN   #{broker_db}.service_instances AS instances ON tables.table_schema = instances.db_name COLLATE utf8_general_ci
-        WHERE  (dbs.Insert_priv = 'N' OR dbs.Update_priv = 'N' OR dbs.Create_priv = 'N')
         GROUP  BY tables.table_schema
         HAVING ROUND(SUM(tables.data_length + tables.index_length) / 1024 / 1024, 1) < MAX(instances.max_storage_mb)
       SQL
