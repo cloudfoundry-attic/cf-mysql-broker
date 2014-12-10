14eca0f6ca5d4f7e3d8ad7a545a0cc4a8c3b108a
Abhijit Hiremagalur and Rob Dimsdale <rdimsdale@pivotal.io>
2014-12-10 10:43:51 -0800

    Make explicit that Database is a singleton

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,18 +1,20 @@
-class Database
-  def self.create(database_name)
+module Database
+  extend self
+
+  def create(database_name)
     connection.execute("CREATE DATABASE IF NOT EXISTS #{connection.quote_table_name(database_name)}")
   end
 
-  def self.drop(database_name)
+  def drop(database_name)
     connection.execute("DROP DATABASE IF EXISTS #{connection.quote_table_name(database_name)}")
   end
 
   # why not "SHOW DATABASES LIKE '#{id}'" ??
-  def self.exists?(database_name)
+  def exists?(database_name)
     1 == connection.select("SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE schema_name=#{connection.quote(database_name)}").rows.first.first
   end
 
-  def self.usage(database_name)
+  def usage(database_name)
     res = connection.select(<<-SQL)
         SELECT ROUND(SUM(data_length + index_length) / 1024 / 1024, 1)
         FROM   information_schema.tables AS tables
@@ -24,11 +26,7 @@ class Database
 
   private
 
-  def self.connection
-    ActiveRecord::Base.connection
-  end
-
   def connection
-    self.class.connection
+    ActiveRecord::Base.connection
   end
 end
