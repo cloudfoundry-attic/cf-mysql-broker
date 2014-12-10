8fbd5445e4d1d6922c28d4038450aff9a4af4956
Abhijit Hiremagalur and Rob Dimsdale <rdimsdale@pivotal.io>
2014-12-09 18:03:26 -0800

    Retrofit unit tests for Quota::Daemon
    Also stopped setting Rails.logger:
    - This causes test pollution/failures
    - This is already being set in config/production.rb
    - [#81697860]

--- lib/quota/daemon.rb
+++ lib/quota/daemon.rb
@@ -4,12 +4,11 @@ module Quota
       dbconfig = YAML.load(File.read(Settings.database_config_path))
       ActiveRecord::Base.establish_connection(dbconfig[Rails.env])
 
-      Rails.logger = Logger.new(STDOUT)
       Enforcer.update_quotas
 
-      loop do
+      Kernel.loop do
         Enforcer.enforce!
-        sleep 1
+        Kernel.sleep(1)
       end
     end
   end
---
+++ spec/lib/quota/daemon_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+module Quota
+  describe Daemon do
+    describe '.start' do
+      before do
+        allow(ActiveRecord::Base).to receive(:establish_connection)
+        allow(Kernel).to receive(:loop)
+        allow(Kernel).to receive(:sleep)
+
+        allow(Enforcer).to receive(:update_quotas)
+        allow(Enforcer).to receive(:enforce!)
+      end
+
+      it 'establishes the initial database connection' do
+        test_db_config = {
+          'adapter' => 'mysql2',
+          'encoding' => 'utf8',
+          'database' => 'test',
+          'pool' =>5,
+          'username' => 'root',
+          'password' =>nil,
+          'host' => 'localhost',
+          'port' =>3306
+        }
+
+        Daemon.start
+        expect(ActiveRecord::Base).to have_received(:establish_connection).with(test_db_config)
+      end
+
+      it 'updates quotas' do
+        Daemon.start
+        expect(Enforcer).to have_received(:update_quotas)
+      end
+
+      it 'enforces quotas once every second' do
+        allow(Kernel).to receive(:loop).and_yield.and_yield
+
+        Daemon.start
+
+        expect(Enforcer).to have_received(:enforce!).twice
+        expect(Kernel).to have_received(:sleep).with(1).twice
+      end
+    end
+  end
+end
