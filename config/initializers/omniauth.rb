b3a9b32b2e7eb15c8372462299dda8275d582d02
Chris Hendrix <chendrix@pivotal.io>
2016-08-22 10:32:06 -0700

    Rename travis_test to ci_test
    [#128688905]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ unless Rails.env.assets?
   client = Settings.services[0].dashboard_client
 
   Rails.application.config.middleware.use OmniAuth::Builder do
-    unless (Rails.env.travis_test? || Rails.env.test? || Rails.env.development?)
+    unless (Rails.env.ci_test? || Rails.env.test? || Rails.env.development?)
       provider :cloudfoundry, client.id, client.secret, {
         auth_server_url: Configuration.auth_server_url,
         token_server_url: Configuration.token_server_url,
