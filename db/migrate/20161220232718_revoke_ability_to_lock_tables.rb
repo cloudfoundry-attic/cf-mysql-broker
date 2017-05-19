e09cad1aab66800517b2e3a3aa56df0f7f1ffd0b
Johanna Smith <josmith@pivotal.io>
2017-05-19 15:18:39 -0600

    % needs to be quoted
    Signed-off-by: Andrew Garner <agarner@pivotal.io>

--- db/migrate/20161220232718_revoke_ability_to_lock_tables.rb
+++ db/migrate/20161220232718_revoke_ability_to_lock_tables.rb
@@ -1,7 +1,7 @@
 class RevokeAbilityToLockTables < ActiveRecord::Migration
   def up
     select_rows("SELECT User, Db, Host FROM mysql.db WHERE Lock_tables_priv='Y'").each do |user, db, host|
-      execute "REVOKE LOCK TABLES ON #{db}.* FROM '#{user}'@'#{host}'"
+      execute "REVOKE LOCK TABLES ON `#{db}`.* FROM '#{user}'@'#{host}'"
     end
   end
 end
