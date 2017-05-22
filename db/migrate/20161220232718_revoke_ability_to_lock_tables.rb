95d71c2bf91dc7135d90a0a000699641ac5c9d07
Your Name <you@example.com>
2017-05-22 10:29:48 -0600

    Revert "Don't let cf-mysql-broker revoke privileges from itself [#144637773]"
    This reverts commit 528c023a466e856153ae86848fedc7785bc8263e.

--- db/migrate/20161220232718_revoke_ability_to_lock_tables.rb
+++ db/migrate/20161220232718_revoke_ability_to_lock_tables.rb
@@ -1,6 +1,6 @@
 class RevokeAbilityToLockTables < ActiveRecord::Migration
   def up
-    select_rows("SELECT User, Db, Host FROM mysql.db WHERE Lock_tables_priv='Y' AND User <> 'cf-mysql-broker'").each do |user, db, host|
+    select_rows("SELECT User, Db, Host FROM mysql.db WHERE Lock_tables_priv='Y'").each do |user, db, host|
       execute "REVOKE LOCK TABLES ON `#{db}`.* FROM '#{user}'@'#{host}'"
     end
   end
