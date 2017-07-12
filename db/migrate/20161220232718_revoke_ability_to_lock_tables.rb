52f4f63762179f633ab11b799c23c27e6f35e6e8
Joseph Palermo <jpalermo@pivotal.io>
2017-07-12 10:42:34 -0700

    Removing revoking of table locks

--- db/migrate/20161220232718_revoke_ability_to_lock_tables.rb
+++
@@ -1,7 +0,0 @@
-class RevokeAbilityToLockTables < ActiveRecord::Migration
-  def up
-    select_rows("SELECT User, Db, Host FROM mysql.db WHERE Lock_tables_priv='Y'").each do |user, db, host|
-      execute "REVOKE LOCK TABLES ON #{db}.* FROM '#{user}'@'#{host}'"
-    end
-  end
-end
