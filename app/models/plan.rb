f87d495b27534aa6648cf49d9ddb4ab42830fcf3
Andy Gilbert <agilbert@pivotal.io>
2019-02-20 12:23:26 -0800

    Backfill plan model tests and fix Plan::to_hash method
    - Test for required keys
    - Add missing keys `max_storage_mb`, `max_user_connections` to to_hash
    
    Signed-off-by: Alvaro Perez-Shirley <aperezshirley@pivotal.io>

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,22 +6,24 @@ class Plan
   end
 
   def initialize(attrs)
-    @id          = attrs.fetch('id')
-    @name        = attrs.fetch('name')
-    @description = attrs.fetch('description')
-    @metadata    = attrs.fetch('metadata', nil)
-    @max_storage_mb = attrs.fetch('max_storage_mb', nil)
+    @id                   = attrs.fetch('id')
+    @name                 = attrs.fetch('name')
+    @description          = attrs.fetch('description')
+    @metadata             = attrs.fetch('metadata', nil)
+    @max_storage_mb       = attrs.fetch('max_storage_mb', nil)
     @max_user_connections = attrs.fetch('max_user_connections', nil)
-    @free        = attrs.fetch('free',true)
+    @free                 = attrs.fetch('free',true)
   end
 
   def to_hash
     {
-      'id'          => self.id,
-      'name'        => self.name,
-      'description' => self.description,
-      'metadata'    => self.metadata,
-      'free'        => self.free,
+      'id'                   => self.id,
+      'name'                 => self.name,
+      'description'          => self.description,
+      'metadata'             => self.metadata,
+      'max_storage_mb'       => self.max_storage_mb,
+      'max_user_connections' => self.max_user_connections,
+      'free'                 => self.free,
     }
   end
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -8,7 +8,7 @@ describe Plan do
         'name'                 => 'plan_name',
         'description'          => 'plan_description',
         'metadata'             => { 'meta_key' => 'meta_value' },
-        'max_storage_mb'       => 5,
+        'max_storage_mb'       => 50,
         'max_user_connections' => 5,
         'free'                 => false,
       )
@@ -17,19 +17,65 @@ describe Plan do
       expect(plan.name).to eq('plan_name')
       expect(plan.description).to eq('plan_description')
       expect(plan.metadata).to eq({ 'meta_key' => 'meta_value' })
-      expect(plan.max_storage_mb).to eq(5)
+      expect(plan.max_storage_mb).to eq(50)
       expect(plan.max_user_connections).to eq(5)
-      expect(plan.free).to be_false
+      expect(plan.free).to be false
+    end
+
+    context 'when the id key is missing' do
+      it 'raises an error' do
+        expect {
+          Plan.build(
+            'name'                 => 'plan_name',
+            'description'          => 'plan_description',
+            'metadata'             => { 'meta_key' => 'meta_value' },
+            'max_storage_mb'       => 50,
+            'max_user_connections' => 5,
+            'free'                 => false,
+          )
+        }.to raise_error(KeyError, 'key not found: "id"')
+      end
+    end
+
+    context 'when the name key is missing' do
+      it 'raises an error' do
+        expect {
+          Plan.build(
+            'id'                   => 'plan_id',
+            'description'          => 'plan_description',
+            'metadata'             => { 'meta_key' => 'meta_value' },
+            'max_storage_mb'       => 50,
+            'max_user_connections' => 5,
+            'free'                 => false,
+          )
+        }.to raise_error(KeyError, 'key not found: "name"')
+      end
+    end
+
+    context 'when the description key is missing' do
+      it 'raises an error' do
+        expect {
+          Plan.build(
+            'id'                   => 'plan_id',
+            'name'                 => 'plan_name',
+            'metadata'             => { 'meta_key' => 'meta_value' },
+            'max_storage_mb'       => 50,
+            'max_user_connections' => 5,
+            'free'                 => false,
+          )
+        }.to raise_error(KeyError, 'key not found: "description"')
+      end
     end
 
     context 'when the metadata key is missing' do
       let(:plan) do
         Plan.build(
-          'id'              => 'plan_id',
-          'name'            => 'plan_name',
-          'description'     => 'plan_description',
-          'max_storage_mb'  => 5,
-          'free'            => false,
+          'id'                   => 'plan_id',
+          'name'                 => 'plan_name',
+          'description'          => 'plan_description',
+          'max_storage_mb'       => 50,
+          'max_user_connections' => 5,
+          'free'                 => false,
         )
       end
 
@@ -41,11 +87,12 @@ describe Plan do
     context 'when the max_storage_mb key is missing' do
       let(:plan) do
         Plan.build(
-            'id'          => 'plan_id',
-            'name'        => 'plan_name',
-            'description' => 'plan_description',
-            'metadata'    => { 'meta_key' => 'meta_value' },
-            'free'        => false,
+          'id'                    => 'plan_id',
+          'name'                  => 'plan_name',
+          'description'           => 'plan_description',
+          'metadata'              => { 'meta_key' => 'meta_value' },
+          'max_user_connections'  => 5,
+          'free'                  => false,
         )
       end
 
@@ -57,11 +104,12 @@ describe Plan do
     context 'when the max_user_connections key is missing' do
       let(:plan) do
         Plan.build(
-          'id'          => 'plan_id',
-          'name'        => 'plan_name',
-          'description' => 'plan_description',
-          'metadata'    => { 'meta_key' => 'meta_value' },
-          'free'        => false,
+          'id'              => 'plan_id',
+          'name'            => 'plan_name',
+          'description'     => 'plan_description',
+          'metadata'        => { 'meta_key' => 'meta_value' },
+          'max_storage_mb'  => 50,
+          'free'            => false,
         )
       end
 
@@ -73,16 +121,17 @@ describe Plan do
     context 'when the free key is missing' do
       let(:plan) do
         Plan.build(
-          'id'              => 'plan_id',
-          'name'            => 'plan_name',
-          'description'     => 'plan_description',
-          'metadata'        => { 'meta_key' => 'meta_value' },
-          'max_storage_mb'  => 5,
+          'id'                    => 'plan_id',
+          'name'                  => 'plan_name',
+          'description'           => 'plan_description',
+          'metadata'              => { 'meta_key' => 'meta_value' },
+          'max_storage_mb'        => 50,
+          'max_user_connections'  => 5,
         )
       end
 
       it 'sets the field to true' do
-        expect(plan.free).to be_true
+        expect(plan.free).to be true
       end
     end
   end
@@ -90,20 +139,22 @@ describe Plan do
   describe '#to_hash' do
     it 'contains the correct values' do
       plan = Plan.new(
-        'id'              => 'plan_id',
-        'name'            => 'plan_name',
-        'description'     => 'plan_description',
-        'metadata'        => { 'key1' => 'value1' },
-        'max_storage_mb'  => 5,
-        'free'            => false,
+        'id'                    => 'plan_id',
+        'name'                  => 'plan_name',
+        'description'           => 'plan_description',
+        'metadata'              => { 'key1' => 'value1' },
+        'max_storage_mb'        => 50,
+        'max_user_connections'  => 5,
+        'free'                  => false,
       )
 
       expect(plan.to_hash.fetch('id')).to eq('plan_id')
       expect(plan.to_hash.fetch('name')).to eq('plan_name')
       expect(plan.to_hash.fetch('description')).to eq('plan_description')
       expect(plan.to_hash.fetch('metadata')).to eq({ 'key1' => 'value1' })
-      expect(plan.to_hash.fetch('max_storage_mb')).to eq(5)
-      expect(plan.to_hash.fetch('free')).to be_false
+      expect(plan.to_hash.fetch('max_storage_mb')).to eq(50)
+      expect(plan.to_hash.fetch('max_user_connections')).to eq(5)
+      expect(plan.to_hash.fetch('free')).to be false
     end
   end
 end
